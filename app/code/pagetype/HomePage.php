3cd381fb3604aa5d7fe08f0afe92980adbf5037e
ishaqidris <sharkyferrari@gmail.com>
2018-11-30 15:23:16 +1300

    removed homepage from this branch

--- app/code/pagetype/HomePage.php
+++ app/code/pagetype/HomePage.php
@@ -9,37 +9,5 @@ use SilverStripe\AssetAdmin\Forms\UploadField;
 
 class HomePage extends Page
 {
-    private static $db = [
-      'BannerText' => 'Varchar',
-    ];
 
-    private static $has_one = [
-        'BannerImage' => Image::class,
-    ];
-
-    private static $owns = [
-        'BannerImage',
-    ];
-
-    public function getCMSFields()
-    {
-        $fields = parent::getCMSFields();
-
-        $fields->addFieldToTab('Root.Banner', TextField::create(
-            'BannerText',
-
-            'Banner text'
-        ));
-
-
-        $fields->addFieldToTab('Root.Banner', $bannerImage = UploadField::create(
-            'BannerImage',
-
-            'Banner image'
-        ));
-
-        $bannerImage->getValidator()->setAllowedExtensions(['jpg','jpeg','png']);
-
-        return $fields;
-    }
 }
