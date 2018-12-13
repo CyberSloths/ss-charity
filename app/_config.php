a56b95db8e0b577d79e456bd1155d6f3cb392d1c
Irsan Arisandy <irsanarisandy@hotmail.com>
2018-12-13 14:42:06 +1300

    Initial UAT Setup on UAT.

--- app/_config.php
+++ app/_config.php
@@ -1,7 +1,9 @@
 <?php
 
-use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
+use SilverStripe\Core\Environment;
+use SilverStripe\FullTextSearch\Solr\Solr;
+use SilverStripe\Security\PasswordValidator;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
@@ -9,10 +11,17 @@ $validator->setMinLength(8);
 $validator->setHistoricCount(6);
 Member::set_password_validator($validator);
 
-# Enable Fulltextsearch
-\SilverStripe\FullTextSearch\Solr\Solr::configure_server([
-    'indexstore' => [
-        'mode' => 'file',
-        'path' => BASE_PATH . '/.solr'
+// Enable Fulltextsearch
+Solr::configure_server(
+    [
+        'host' => Environment::getEnv('SOLR_SERVER') ? : 'localhost',
+        'port' => Environment::getEnv('SOLR_PORT') ? : '8983',
+        'path' => Environment::getEnv('SOLR_PATH') ? : '/solr',
+        'indexstore' => [
+            'mode' => Environment::getEnv('SOLR_MODE') ? : 'file',
+            'auth' => Environment::getEnv('SOLR_AUTH') ? : null,
+            'path' => Environment::getEnv('SOLR_INDEXSTORE_PATH') ? : BASE_PATH.'/.solr',
+            'remotepath' => Environment::getEnv('SOLR_REMOTE_PATH') ? : null,
+        ]
     ]
-]);
+);
