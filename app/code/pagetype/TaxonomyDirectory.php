a86a43e6ffd5697ecfaa11eb2341d25966ec98b1
Ishaq Idris <sharkyferrari@gmail.com>
2018-12-18 10:56:03 +1300

    removed styling

--- app/code/pagetype/TaxonomyDirectory.php
+++ app/code/pagetype/TaxonomyDirectory.php
@@ -3,49 +3,36 @@
 namespace App\PageType;
 
 use Page;
+use App\PageType\NewsPage;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\GraphQL\Controller;
 use SilverStripe\Taxonomy\TaxonomyTerm;
 use App\Controller\CustomTaxonomyDirectoryController;
-use SilverStripe\GraphQL\Controller;
 
 class TaxonomyDirectory extends Page
 {
+    /**
+     * Default children of the Taxonomy Directory
+     *
+     * @var string
+     */
+    private static $default_child = NewsPage::class;
+
+    /**
+     * Children allowed
+     *
+     * @var array
+     */
+    private static $allowed_children = [
+        NewsPage::class,
+    ];
+
     /**
      * Page description in CMS
      *
      * @var string
      */
     private static $description = 'Page for listing news';
-
-    /**
-     * Stage children for the listing page based of terms
-     *
-     * @param boolean $showAll Flag to show all children pages
-     *
-     * @return void
-     */
-    public function stageChildren($showAll = false)
-    {
-
-        $termIDs = TaxonomyTerm::get()->getIDList();
-
-        return Page::get()
-            ->exclude('ID', $this->ID)
-            ->filter(['Terms.ID' => $termIDs]);
-    }
-
-    /**
-     * Pass all children of the listing page to stage step
-     *
-     * @param boolean $showAll              Flag to show all children pages
-     * @param boolean $onlyDeletedFromStage Pages not related to term are removed
-     *
-     * @return void
-     */
-    public function liveChildren($showAll = false, $onlyDeletedFromStage = false)
-    {
-        return $this->stageChildren($showAll);
-    }
 
     /**
      * Set controller for the page to use
