5808d7c2b0213164117c1c188d11b2050a1a5028
Ishaq Idris <sharkyferrari@gmail.com>
2018-12-20 09:07:51 +1300

    fixed arrow hover

--- app/code/extension/SiteConfigExtension.php
+++ app/code/extension/SiteConfigExtension.php
@@ -27,6 +27,7 @@ class SiteConfigExtension extends DataExtension
         'FooterSentence2' => 'Varchar',
         'AlertTitle' => 'Varchar',
         'AlertContent' => 'Text',
+        'FacebookLink' => 'Varchar',
     ];
 
     /**
@@ -72,7 +73,10 @@ class SiteConfigExtension extends DataExtension
                 ), TextField::create(
                     'Facebook',
                     'Facebook'
-                )
+                ), TextField::create(
+                    'FacebookLink',
+                    'Facebook Link'
+                )->setDescription('Full link to facebook page required.')
             ]
         );
 
